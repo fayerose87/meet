aefc9e35f0bf1204e27cbe780c54d6b978033b61
fayerose87 <79806977+fayerose87@users.noreply.github.com>
2021-09-22 21:03:17 -0500

    added offline capability

--- src/api.js
+++ src/api.js
@@ -32,7 +32,13 @@ const checkToken = async (accessToken) => {
     if (window.location.href.startsWith("http://localhost")) {
      NProgress.done();
       return mockData;
-  }
+    }
+
+    if (!navigator.onLine) {
+      const data = localStorage.getItem("lastEvents");
+      NProgress.done();
+       return data?JSON.parse(events).events:[];;
+    }
 
 const token = await getAccessToken();
 
