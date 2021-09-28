6dae28d614acdfbc5e0d93bfadd1fb2555806b24
fayerose87 <79806977+fayerose87@users.noreply.github.com>
2021-09-27 21:05:31 -0500

    minor fixes

--- src/App.js
+++ src/App.js
@@ -89,7 +89,7 @@ class App extends Component {
 
 
   render() {
-    const { locations, numberOfEvents, events, offlineAlert } = this.state;
+    const { offlineAlert } = this.state;
     if (this.state.showWelcomeScreen === undefined) return <div className="App" />
     return (
       <div className="App">
@@ -125,7 +125,7 @@ class App extends Component {
               <CartesianGrid />
               <XAxis type="category" dataKey="city" name="city" />
               <YAxis type="number" dataKey="number" name="number of events" />
-                Tooltip cursor={{ strokeDasharray: '3 3' }} />
+                <Tooltip cursor={{ strokeDasharray: '3 3' }} />
               <Scatter data={this.getData()} fill="#8884d8" />
             </ScatterChart>
           </ResponsiveContainer>
