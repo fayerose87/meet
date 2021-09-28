e03ee82929208cde2a0818265c0471edbdbb2670
fayerose87 <79806977+fayerose87@users.noreply.github.com>
2021-09-27 21:03:11 -0500

    added scatter chart

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import logo from './devmeet_logo.png';
 import header from './header_image.jpg';
 import { OfflineAlert } from './Alert';
 import WelcomeScreen from './WelcomeScreen';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 class App extends Component {
   state = {
@@ -75,11 +76,20 @@ class App extends Component {
     this.updateEvents(currentLocation, eventNumber);
   }
 
+  getData = () => {
+    const {locations, events} = this.state;
+    const data = locations.map((location)=>{
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(', ').shift()
+      return {city, number};
+    })
+    return data;
+  };
 
 
 
   render() {
-    const { offlineAlert } = this.state;
+    const { locations, numberOfEvents, events, offlineAlert } = this.state;
     if (this.state.showWelcomeScreen === undefined) return <div className="App" />
     return (
       <div className="App">
@@ -96,6 +106,7 @@ class App extends Component {
              <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} events={this.state.events}/>
           </Container>
       </Navbar>
+
       <img 
         alt="header"
         src={header}
@@ -106,9 +117,23 @@ class App extends Component {
       <OfflineAlert text={offlineAlert}/>
 
         <Container fluid>
-          <NumberOfEvents updateNumberOfEvents={(e) => this.updateNumberOfEvents(e)} /> 
+
+          <NumberOfEvents updateNumberOfEvents={(e) => this.updateNumberOfEvents(e)} />
+
+          <ResponsiveContainer height={400} >
+            <ScatterChart margin={{top: 20, right: 20, bottom: 20, left: 20,}}>
+              <CartesianGrid />
+              <XAxis type="category" dataKey="city" name="city" />
+              <YAxis type="number" dataKey="number" name="number of events" />
+                Tooltip cursor={{ strokeDasharray: '3 3' }} />
+              <Scatter data={this.getData()} fill="#8884d8" />
+            </ScatterChart>
+          </ResponsiveContainer>
+
           <EventList events={this.state.events}/>
+
         </Container>
+
         <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen} getAccessToken={() => { getAccessToken() }} />
       </div>
     );
